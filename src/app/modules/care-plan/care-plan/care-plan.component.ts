d4e3b22c883e31b482f72099c2b699130a54261e
Nabeel Abdul Aziz Khan <nabeel.khan@novizant.com>
2024-10-30 18:30:58 +0530

    Buttons disabled

--- src/app/modules/care-plan/care-plan/care-plan.component.ts
+++ src/app/modules/care-plan/care-plan/care-plan.component.ts
@@ -8,7 +8,7 @@ import { MilestonePopupComponent } from '../../milestone-popup/milestone-popup.c
 import { GoalPopupComponent } from '../../goal-popup/goal-popup.component';
 import { ActivatedRoute } from '@angular/router';
 import { CarePlanService } from '../../services/care-plan-service.service';
-import { take } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 
 interface TreeNode {
@@ -40,8 +40,16 @@ export class CarePlanComponent {
   assessmentId!: number;
 
   readonly panelOpenState = signal(false);
-  carePlanModel$ = this.carePlanService.currentCarePlanModel;
- 
+  hasCaseName$ = this.carePlanService.currentCarePlanModel.pipe(
+    map((carePlanModel) => !!carePlanModel?.caseName)
+  );
+  hasProblems$ = this.carePlanService.currentCarePlanModel.pipe(
+    map((carePlanModel) => !!carePlanModel?.problems?.length)
+  );
+  hasGoal$ = this.carePlanService.currentCarePlanModel.pipe(
+    map((carePlanModel) =>!!carePlanModel?.problems?.flatMap((problem) => problem.goals)?.length)
+  )
+
   constructor(private dialog: MatDialog,private route: ActivatedRoute,private carePlanService: CarePlanService) {
   }
   ngOnInit() {
@@ -59,7 +67,7 @@ export class CarePlanComponent {
   opeCaseDialog(): void {
     const dialogRef = this.dialog.open(CasePopupComponent, {
       width: '600px',
-      data: {}, // Optional: Pass data if needed
+      data: {}, 
     });
 
     dialogRef.afterClosed().subscribe((result) => {
@@ -69,12 +77,6 @@ export class CarePlanComponent {
 
 
 opeProblemDialog(): void {
-  this.carePlanModel$.pipe(take(1)).subscribe((carePlanModel) => {
-    if (!carePlanModel || !carePlanModel.caseName) {
-      alert("Please select a case");
-      this.opeCaseDialog(); 
-      return;
-    }
     const dialogRef = this.dialog.open(ProblemPopupComponent, {
       width: '600px',
       data: {}, 
@@ -83,48 +85,30 @@ opeProblemDialog(): void {
     dialogRef.afterClosed().subscribe((result) => {
     //  console.log('Dialog closed, selected country:', result);
     });
-  });
+
   
 }
 
   
 opeGoalDialog(): void {
-  this.carePlanModel$.pipe(take(1)).subscribe((carePlanModel) => {
-    if (!carePlanModel || carePlanModel.problems.length === 0) {
-      alert("Please select a case with problems");
-      this.opeProblemDialog();
-      return; 
-    }
+  const dialogRef = this.dialog.open(GoalPopupComponent, {
+    width: '600px',
+    data: {}, 
+  });
 
-    const dialogRef = this.dialog.open(GoalPopupComponent, {
-      width: '600px',
-      data: {}, 
-    });
-  
-    dialogRef.afterClosed().subscribe((result) => {
-    //  console.log('Dialog closed, selected country:', result);
-    });
+  dialogRef.afterClosed().subscribe((result) => {
+  //  console.log('Dialog closed, selected country:', result);
   });
 }
 
 opeMilestoneDialog(): void {
-  this.carePlanModel$.pipe(take(1)).subscribe((carePlanModel) => {
-    const hasGoals = carePlanModel?.problems.some(problem => problem.goals.length > 0);
-    
-    if (!hasGoals) {
-      alert("Please select a case with goals");
-      this.opeGoalDialog();
-      return; 
-    }
+  const dialogRef = this.dialog.open(MilestonePopupComponent, {
+    width: '600px',
+    data: {}, 
+  });
 
-    const dialogRef = this.dialog.open(MilestonePopupComponent, {
-      width: '600px',
-      data: {}, 
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-   //   console.log('Dialog closed, selected country:', result);
-    });
+  dialogRef.afterClosed().subscribe((result) => {
+ //   console.log('Dialog closed, selected country:', result);
   });
 }
 
